f7b2c593733c5f7518e6bc75f2113e9fe0ed1ab7
Harindu973 <harindulakshankob@gmail.com>
2020-07-15 00:20:42 +0530

    Created Components and Updated navbar

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-//import React from 'react';
-//import logo from "./logo.svg";
-//import "./App.css";
-import React, { useState } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap";
+import React, { Component } from "react";
 
-const App = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
+//components
+import Header from "./components/header/header";
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
-  return (
-    <div className="App">
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">
-          Satriaana
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="https://fossnsbm.org">Blog</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://forum.fossnsbm.org">Forum</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/contact">Contact Us</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/about">About Us</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/donations">Donations</NavLink>
-            </NavItem>
-
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
 
 export default App;
