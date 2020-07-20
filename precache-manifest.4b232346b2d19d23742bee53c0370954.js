d512c22e87746a599fbed3e82d2e0434a3692552
Buddhika <buddhiextuse@gmail.com>
2020-07-21 04:26:45 +0530

    Updates

---
+++ precache-manifest.4b232346b2d19d23742bee53c0370954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572d5abaee510b349db27741de115cee",
+    "url": "/Satriaana-Web-Site/index.html"
+  },
+  {
+    "revision": "838d69761da5c878c86b",
+    "url": "/Satriaana-Web-Site/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "eca89a467bb0b095f1b2",
+    "url": "/Satriaana-Web-Site/static/css/main.25d8078b.chunk.css"
+  },
+  {
+    "revision": "838d69761da5c878c86b",
+    "url": "/Satriaana-Web-Site/static/js/2.50772fe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Satriaana-Web-Site/static/js/2.50772fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca89a467bb0b095f1b2",
+    "url": "/Satriaana-Web-Site/static/js/main.456ee330.chunk.js"
+  },
+  {
+    "revision": "d00cdac9d17062a70cea",
+    "url": "/Satriaana-Web-Site/static/js/runtime-main.a6134674.js"
+  }
+]);
