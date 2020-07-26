6d3aad1eddada090b2f6099090e567ba8b7404a4
Buddhika <buddhikadananjaya55@gmail.com>
2020-07-26 16:12:58 +0530

    edit cover js and css file

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -4,9 +4,11 @@ import "./cover.css";
 class Cover extends Component {
     render() {
         return (
-            <div class="covr">
-                <div class="h1">
-                We code for open-privacy
+            <div className="covr">
+                <div className="container">
+                    <div className="col-md-12">
+                        <h1 className="new1">We code For Open-Privacy</h1>
+                    </div>
                 </div>
             </div>
         );
