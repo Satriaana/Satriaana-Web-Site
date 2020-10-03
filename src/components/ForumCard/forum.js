05349595cf7a9e7174a115563abe1aea31844527
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2020-10-03 19:09:59 +0530

    Fixed text-align issues

--- src/components/ForumCard/forum.js
+++ src/components/ForumCard/forum.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./forum.css";
 import forumImage from "../../assets/nordwood-themes-bJjsKbToY34-unsplash.png";
 
 class Forum extends Component {
@@ -8,30 +9,30 @@ class Forum extends Component {
         <div class="container" style={{paddingTop: '50px'}}>
             <div class="row">
                 <div class="col-md-12" style={{marginBottom: '10px'}}>
-                    <h1 style={{fontSize: '40px', fontFamily: 'Kumbh Sans', fontWeight: 'bold'}}>Forum<button class="btn btn-primary float-right" type="button">Lorem Ipsum</button></h1>
-                    <p style={{fontSize: '21px', fontFamily: 'Kumbh Sans', color: '#21262b'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,&nbsp;<br/>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/></p>
+                    <h1 id="title" style={{fontSize: '40px', fontFamily: 'Kumbh Sans', fontWeight: 'bold'}}>Forum<button class="btn btn-primary float-right" type="button">Lorem Ipsum</button></h1>
+                    <p id="desc" style={{fontSize: '21px', fontFamily: 'Kumbh Sans', color: '#21262b'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,&nbsp;<br/>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/></p>
                 </div>
             </div>
             <div class="row" style={{paddingBottom: '50px'}}>
                 <div class="col-md-4">
-                    <div class="card" style={{borderRadius: '10px', marginBottom: '30px'}}><img class="card-img w-100 d-block" src={forumImage} style={{objectFit:"cover",height: '320px', borderRadius: '10px'}}/>
-                        <div class="card-img-overlay" style={{background: 'url(./nordwood-themes-bJjsKbToY34-unsplash.png) center / cover no-repeat', borderRadius: '10px'}}>
+                    <div class="card" style={{borderRadius: '10px', marginBottom: '30px'}}><img alt="" class="card-img w-100 d-block" src={forumImage} style={{objectFit:"cover",height: '320px', borderRadius: '10px'}}/>
+                        <div class="card-img-overlay">
                             <h4 style={{color: 'rgb(255,255,255)', textShadow: '0px 0px 1px'}}>Recent Thread #1</h4>
                             <p style={{color: 'rgb(255,255,255)', textShadow: '0px 0px 1px'}}><a href="#">Visit &gt;</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="card" style={{borderRadius: '10px', marginBottom: '30px'}}><img class="card-img w-100 d-block" src={forumImage} style={{objectFit:"cover",height: '320px', borderRadius: '10px'}}/>
-                        <div class="card-img-overlay" style={{background: 'url(./nordwood-themes-bJjsKbToY34-unsplash.png) center / cover no-repeat', borderRadius: '10px'}}>
+                    <div class="card" style={{borderRadius: '10px', marginBottom: '30px'}}><img alt="" class="card-img w-100 d-block" src={forumImage} style={{objectFit:"cover",height: '320px', borderRadius: '10px'}}/>
+                        <div class="card-img-overlay">
                             <h4 style={{color: 'rgb(255,255,255)', textShadow: '0px 0px 1px'}}>Recent Thread #2</h4>
                             <p style={{color: 'rgb(255,255,255)', textShadow: '0px 0px 1px'}}><a href="#">Visit &gt;</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="card" style={{borderRadius: '10px', marginBottom: '30px'}}><img class="card-img w-100 d-block" src={forumImage} style={{objectFit:"cover",height: '320px', background: 'url(./nordwood-themes-bJjsKbToY34-unsplash.png) center / cover no-repeat', borderRadius: '10px'}}/>
-                        <div class="card-img-overlay" style={{borderRadius: '10px'}}>
+                    <div class="card" style={{borderRadius: '10px', marginBottom: '30px'}}><img alt="" class="card-img w-100 d-block" src={forumImage} style={{objectFit:"cover",height: '320px', background: 'url(./nordwood-themes-bJjsKbToY34-unsplash.png) center / cover no-repeat', borderRadius: '10px'}}/>
+                        <div class="card-img-overlay">
                             <h4 style={{color: 'rgb(255,255,255)', textShadow: '0px 0px 1px'}}>Recent Thread #3</h4>
                             <p style={{color: 'rgb(255,255,255)', textShadow: '0px 0px 1px'}}><a href="#">Visit &gt;</a></p>
                         </div>
--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -6,7 +6,7 @@ class Cover extends Component {
         return (
             <div class="cover">
                     <div className="container-fluid">
-                        <h4 style={{fontFamily: 'Kumbh Sans', fontWeight: 'bold', paddingTop: '30px', paddingBottom: '25px', marginBottom: '0'}}>Interested in Joining Satriaana as a Volunteer?&nbsp;&nbsp;&nbsp;<a href="https://satriaana.typeform.com/to/XYGqfnkI" target="_blank"><button class="btn btn-primary" type="button">Get started</button></a></h4>
+                        <h4 style={{fontFamily: 'Kumbh Sans', fontWeight: 'bold', paddingTop: '30px', paddingBottom: '25px', marginBottom: '0'}}>Interested in Joining Satriaana as a Volunteer?&nbsp;&nbsp;&nbsp;<a href="https://satriaana.typeform.com/to/XYGqfnkI" target="_blank" rel="noopener noreferrer"><button class="btn btn-primary" type="button">Get started</button></a></h4>
                     </div>
                 </div>
         );
