66bd96dda95cf8351cc2afd417877cbc2ca25a7b
BuddhikaD <buddhiextuse@gmail.com>
2020-10-04 20:39:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Satriaana-Web-Site/precache-manifest.2070c8768ac9edf006413835471e30f0.js"
+  "/Satriaana-Web-Site/precache-manifest.4999fbe41b7cee09f3ea75bbb36886bc.js"
 );
 
 self.addEventListener('message', (event) => {
