6419f5d7194de4e17fbf8c60684f4c4bfaf6d664
pankaj <gupta811pankaj811@gmail.com>
2020-10-04 13:06:57 +0530

    got latest from dev in header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,89 +1,84 @@
-import React from "react";
-import "./toggleTheme.css";
+import React, { Component } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 import HeaderLogo from "../../assets/satriaana-logo.png";
 
-import ToggleMode from '../Services/toggleMode'
-const  header =  ()=>{
-  return (
-    <header>
-      <nav
-        className="navbar navbar-light navbar-expand-md"
-        style={{
-          fontFamily: "Kumbh Sans",
-          fontWeight: "bold",
-          fontSize: "18px",
-        }}
-      >
-        <div className="container-fluid">
-          <img
-            alt
-            src={HeaderLogo}
-            style={{
-              width: "130px",
-            }}
-          />
-          <a className="navbar-brand" href="#"></a>
-          <button
-            data-toggle="collapse"
-            className="navbar-toggler"
-            data-target="#navcol-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse d-md-flex d-lg-flex justify-content-md-end justify-content-lg-end"
-            id="navcol-1"
-          >
-            <ul className="nav navbar-nav">
-              {/* {this.props.toggleMode ? "Working " : "Not Working"} */}
-              <Link style={{ textDecoration: "none" }} to="/Satriaana-Web-Site">
+class header extends Component {
+  render() {
+    return (
+      <header>
+        <nav
+          className="navbar navbar-light navbar-expand-md"
+          style={{
+            fontFamily: "Kumbh Sans",
+            fontWeight: "bold",
+            fontSize: "18px",
+          }}
+        >
+          <div className="container-fluid">
+            <a
+              className="navbar-brand"
+              href="https://satriaana.github.io/Satriaana-Web-Site/"
+            >
+              <img
+                alt
+                src={HeaderLogo}
+                style={{
+                  width: "130px",
+                }}
+              />
+            </a>
+            <button
+              data-toggle="collapse"
+              className="navbar-toggler"
+              data-target="#navcol-1"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse d-md-flex d-lg-flex justify-content-md-end justify-content-lg-end"
+              id="navcol-1"
+            >
+              <ul className="nav navbar-nav">
+                <Link
+                  style={{ textDecoration: "none" }}
+                  to="/Satriaana-Web-Site"
+                >
+                  {" "}
+                  <li className="nav-item">
+                    <a className="nav-link" href="/Satriaana-Web-Site">
+                      Home
+                    </a>
+                  </li>
+                </Link>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Home
+                  <a className="nav-link" href="https://blog.satriaana.org">
+                    Blog
                   </a>
                 </li>
-              </Link>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Blog
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Forum
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/contact">
-                  Contact
-                </a>
-              </li>
-              <Link style={{ textDecoration: "none" }} to="/about">
                 <li className="nav-item">
-                  <a className="nav-link">About</a>
+                  <a className="nav-link" href="https://forum.satriaana.org">
+                    Forum
+                  </a>
                 </li>
-              </Link>
-              {/* <button
-                type="button"
-                onClick={toggle}
-                style={{
-                  backgroundColor: theme.backgroundColor,
-                  color: theme.color,
-                  outline: "none",
-                }}
-              >
-                Toggle to {!dark ? "Dark" : "Light"} theme
-              </button> */}
-              <ToggleMode/>
-            </ul>
+                <li className="nav-item">
+                  <a className="nav-link" href="/contact">
+                    Contact
+                  </a>
+                </li>
+                <Link style={{ textDecoration: "none" }} to="/about">
+                  <li className="nav-item">
+                    <a className="nav-link">About</a>
+                  </li>
+                </Link>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
-  );
-};
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default header;
