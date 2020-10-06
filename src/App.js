3c1d1c43c670e28366fc21595dd08092b8d9b184
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-10-06 11:59:40 +0545

    Fix base public url not being appended on route

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const Home = () => {
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Header />
           <Switch>
