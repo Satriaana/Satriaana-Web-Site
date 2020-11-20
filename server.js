5dc1f5bcdac5b5bc53e826129e1a0f5f78973571
Uvindu Shehan Perera <44149708+uvinduperera@users.noreply.github.com>
2020-11-20 13:14:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
-/*'use strict';
+const express = require('express')
+const path = require('path')
 
-const express = require('express');
+const httpPort = 80
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const app = express()
 
-// App
-const app = express();
-app.post('/', function (req, res) {
-  res.send('Hello World');
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
-*/
-	
-var express = require("express");
- 
-var app = express();
- 
-app.use(express.static('public'));
- 
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/src/css'));
-app.use('/js', express.static(__dirname + '/src/js'));
-app.use('/assets', express.static(__dirname + '/src/assets'));
-app.use('/json', express.static(__dirname + '/public')); 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
-var server = app.listen(8080, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-});
+app.listen(httpPort, function () {
+  console.log(`Listening on port ${httpPort}!`)
+})
